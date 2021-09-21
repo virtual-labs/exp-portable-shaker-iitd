1420ffe44bde2b534063b38b2fdb0bc4962ad2fc
Virtual Labs IIT Delhi <71967438+vlabsdev-iitd@users.noreply.github.com>
2021-09-21 10:38:22 +0530

    Update pretest.js

--- experiment/pretest.js
+++ experiment/pretest.js
@@ -100,7 +100,7 @@
         },
 
         {
-            question: "2. The beam vibrates, the surface strain fluctuates between _____ and _____", ///// Write the question inside double quotes
+            question: "2. The beam vibrates, the surface strain fluctuates between", ///// Write the question inside double quotes
             answers: {
                 a: "Compression & tension", ///// Write the option 1 inside double quotes
                 b: "Voltage & time", ///// Write the option 2 inside double quotes
