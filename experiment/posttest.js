44408d378bd26e2a1f87a506a4e4f3f024b50d8d
Virtual Labs IIT Delhi <71967438+vlabsdev-iitd@users.noreply.github.com>
2021-09-21 12:24:25 +0530

    Update posttest.js

--- experiment/posttest.js
+++ experiment/posttest.js
@@ -100,14 +100,14 @@
         },
 
         {
-            question: "2. The split tensile strength of the cylindrical concrete specimen is given by?", ///// Write the question inside double quotes
+            question: "2. The instantaneous voltage developed across the piezoelectric sensor can be measured at the user specified time interval using the ?", ///// Write the question inside double quotes
             answers: {
-                a: "<img src='images/post5.png'/>", ///// Write the option 1 inside double quotes
-                b: "<img src='images/post6.png'/>", ///// Write the option 2 inside double quotes
-                c: "<img src='images/post7.png'/>", ///// Write the option 1 inside double quotes
-                d: "<img src='images/post8.png'/>", ///// Write the option 2 inside double quotes
+                a: "Beam", ///// Write the option 1 inside double quotes
+                b: "PZT Patch", ///// Write the option 2 inside double quotes
+                c: "Digital multi meter", ///// Write the option 1 inside double quotes
+                d: "Function generator,", ///// Write the option 2 inside double quotes
             },
-            correctAnswer: "a" ///// Write the correct option inside double quotes
+            correctAnswer: "c" ///// Write the correct option inside double quotes
         },
 
         {
